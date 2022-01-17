731357c6aede9fc09012b75e596f88947e6e62b1
김동규 <tkillman@logiall.com>
2022-01-17 17:20:30 +0900

    first commit

---
+++ src/main/java/com/example/springkafkadong/SpringKafkaDongApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkadong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaDongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaDongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springkafkadong/SpringKafkaDongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkadong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaDongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
